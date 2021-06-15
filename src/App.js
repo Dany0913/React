ba8f1c30deb75414dd71fb8cc554f5fda4cc0ad6
Daniela <dany0913@hotmail.com>
2021-06-15 18:49:54 -0500

    Correct h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Mi primera aplicación1</h1>
+      <h1>Mi primera aplicación</h1>
       <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
